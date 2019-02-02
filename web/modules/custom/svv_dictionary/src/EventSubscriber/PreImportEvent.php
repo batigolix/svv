3b0b90cab537649a86be5885aefa1a0e085e35c5
Boris Doesborg <boris.doesborg@finalist.nl>
2019-02-02 20:39:30 +0100

    improves dictionary import

---
+++ web/modules/custom/svv_dictionary/src/EventSubscriber/PreImportEvent.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Drupal\svv_dictionary\EventSubscriber;
+
+use Drupal\migrate\Event\MigrateEvents;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+ * Class PreImportEvent
+ *
+ * @package Drupal\svv_dictionary\EventSubscriber
+ */
+class PreImportEvent implements EventSubscriberInterface {
+
+  /**
+   * @return mixed
+   */
+  public static function getSubscribedEvents() {
+    $events[MigrateEvents::PRE_IMPORT][] = [
+      'preImport',
+      0,
+    ];
+    return $events;
+  }
+
+  /**
+   * @param $event
+   */
+  public function preImport($event) {
+    $path = 'public://svv_dictionary/';
+    file_prepare_directory($path, FILE_CREATE_DIRECTORY);
+    $config = \Drupal::service('config.factory')
+      ->getEditable('svv_dictionary.config');
+    $url = $config->get('url_to_google_docs_csv_file');
+    $result = system_retrieve_file($url, 'public://svv_dictionary/dictionary.csv', FALSE, FILE_EXISTS_REPLACE);
+  }
+}
---
+++ web/modules/custom/svv_dictionary/src/Form/ConfigForm.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Drupal\svv_dictionary\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Class ConfigForm.
+ */
+class ConfigForm extends ConfigFormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    return [
+      'svv_dictionary.config',
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'config_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $config = $this->config('svv_dictionary.config');
+    $form['url_to_google_docs_csv_file'] = [
+      '#type' => 'url',
+      '#title' => $this->t('URL to google docs CSV file'),
+      '#description' => $this->t('Comma separated values file that holds the dictionary.'),
+      '#default_value' => $config->get('url_to_google_docs_csv_file'),
+    ];
+    return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    parent::validateForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    parent::submitForm($form, $form_state);
+
+    $this->config('svv_dictionary.config')
+      ->set('url_to_google_docs_csv_file', $form_state->getValue('url_to_google_docs_csv_file'))
+      ->save();
+  }
+
+}
