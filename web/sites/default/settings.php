7753a56be01fb5f22895f5547df5ed41d911f148
Boris Doesborg <boris.doesborg@finalist.nl>
2022-09-13 08:29:51 +0200

    wip

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -776,3 +776,9 @@ if (strpos(DRUPAL_ROOT, 'stage')) {
 }
 
 $settings["config_sync_directory"] = '../config/sync';
+
+// Automatically generated include for settings managed by ddev.
+$ddev_settings = dirname(__FILE__) . '/settings.ddev.php';
+if (getenv('IS_DDEV_PROJECT') == 'true' && is_readable($ddev_settings)) {
+  require $ddev_settings;
+}
